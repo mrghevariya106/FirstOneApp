6ba68f40410c217eaae896df0964369b9cb760ca
maypatel_107 <mrghevariya107@gmail.com>
2021-09-16 23:03:27 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          Hello World!
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
